d2874078da95245db054d3aff4fc0597667e657f
dienelopes <dienelopes7@gmail.com>
2022-11-14 12:03:17 -0300

    Adicionando tabela no container

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ $resultado = $conexao -> query($consulta);
 ?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
-<div>
+<div class="container" style="margin-top: 20px">
     <h1>Alunos</h1>
     <hr>
-    <table border='1' class="table table-striped table-hover">
+    <table border='1' class="table table-striped table-secondary">
         <thead class="table-dark">
             <tr>
                 <th>Nome</th>
